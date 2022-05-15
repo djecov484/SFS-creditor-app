1c65de09c53c7ae8975fa04ba1b44ddb008ce6f2
djecov484 <djecov484@gmail.com>
2022-05-15 09:05:14 -0700

    added some unit tests

--- cypress/integration/Table.spec.js
+++ cypress/integration/Table.spec.js
@@ -1,10 +1,26 @@
-{/* <reference types="cypress" /> */}
-
-describe("Table", () =>{
-    it( "verify creditor from anywere in the table", function (){
+describe("UI Tests", function () {
+    it( " load successfully", function (){
         cy.visit("http://localhost:3000/")
         
-          
+        // find first element by Creditor  Name
+        cy.contains("CAPITAL ONE").click()
+   
+        // click on element containg "REMOVE DEPT"
+        cy.contains("REMOVE DEBT").click() 
      
+        //find children of the Table
+        cy.get("table").children()
+
+        
     })
+      //List with first item
+    it("Checks texts of table items", function () {
+        cy
+          .get("table")
+          .eq(0)
+          .should("contain.text", "Creditor");
+
+        
+    })
+    
 })
